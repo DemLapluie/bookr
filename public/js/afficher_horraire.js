8318df7c170a8ab54df9826c3ae5e04175982ee5
ikir85 <riki.tinoza@hotmail.com>
2018-08-28 17:32:43 +0200

    profil -jours

--- public/js/afficher_horraire.js
+++ public/js/afficher_horraire.js
@@ -1,11 +1,11 @@
 
-$("#inscription_prestataire_jour_0").click(function(){ afficherHorraire(); });
-$("#inscription_prestataire_jour_1").click(function(){ afficherHorraire(); });
-$("#inscription_prestataire_jour_2").click(function(){ afficherHorraire(); });
-$("#inscription_prestataire_jour_3").click(function(){ afficherHorraire(); });
-$("#inscription_prestataire_jour_4").click(function(){ afficherHorraire(); });
-$("#inscription_prestataire_jour_5").click(function(){ afficherHorraire(); });
-$("#inscription_prestataire_jour_6").click(function(){ afficherHorraire(); });
+$("#profil_prestataire_jour_0").click(function(){ afficherHorraire(); });
+$("#profil_prestataire_jour_1").click(function(){ afficherHorraire(); });
+$("#profil_prestataire_jour_2").click(function(){ afficherHorraire(); });
+$("#profil_prestataire_jour_3").click(function(){ afficherHorraire(); });
+$("#profil_prestataire_jour_4").click(function(){ afficherHorraire(); });
+$("#profil_prestataire_jour_5").click(function(){ afficherHorraire(); });
+$("#profil_prestataire_jour_6").click(function(){ afficherHorraire(); });
 
 $("#inscription_prestataire_jour_0").parent().append("<div id='add'></div>");
 $("#inscription_prestataire_jour_1").parent().append("<div id='add1'></div>");
@@ -18,73 +18,87 @@ $("#inscription_prestataire_jour_6").parent().append("<div id='add6'></div>");
 
 function afficherHorraire()
 {
-    if ($("#inscription_prestataire_jour_0").is(":checked")) {
-        document.getElementById("add").style.height = "auto";
-        document.getElementById("add").append(document.getElementById("horraire-lundi"));
-        document.getElementById("horraire-lundi").style.visibility = "visible";
+    if ($("#profil_prestataire_jour_0").is(":checked")) {
+        /*document.getElementById("add").style.height = "auto";
+        document.getElementById("add").append(document.getElementById("horaire-lundi"));*/
+        document.getElementById("horaire-lundi").style.visibility = "visible";
+        document.getElementById("horaire-lundi").style.display = "table-row";
     }
-    if (!$("#inscription_prestataire_jour_0").is(":checked")) {
-        document.getElementById("horraire-lundi").style.visibility = "hidden";
-        document.getElementById("add").style.height = "0";
-    }
-
-    if ($("#inscription_prestataire_jour_1").is(":checked")) {
-        document.getElementById("add1").style.height = "auto";
-        document.getElementById("add1").append(document.getElementById("horraire-mardi"));
-        document.getElementById("horraire-mardi").style.visibility = "visible";
-    }
-    if (!$("#inscription_prestataire_jour_1").is(":checked")) {
-        document.getElementById("horraire-mardi").style.visibility = "hidden";
-        document.getElementById("add1").style.height = "0";
+    if (!$("#profil_prestataire_jour_0").is(":checked")) {
+        document.getElementById("horaire-lundi").style.visibility = "hidden";
+        document.getElementById("horaire-lundi").style.height = "0";
+        document.getElementById("horaire-lundi").style.display = "none";
     }
 
-    if ($("#inscription_prestataire_jour_2").is(":checked")) {
-        document.getElementById("add2").style.height = "auto";
-        document.getElementById("add2").append(document.getElementById("horraire-mercredi"));
-        document.getElementById("horraire-mercredi").style.visibility = "visible";
+    if ($("#profil_prestataire_jour_1").is(":checked")) {
+        /*document.getElementById("add1").style.height = "auto";
+        document.getElementById("add1").append(document.getElementById("horaire-mardi"));*/
+        document.getElementById("horaire-mardi").style.visibility = "visible";
+        document.getElementById("horaire-mardi").style.display = "table-row";
     }
-    if (!$("#inscription_prestataire_jour_2").is(":checked")) {
-        document.getElementById("horraire-mercredi").style.visibility = "hidden";
-        document.getElementById("add2").style.height = "0";
-    }
-
-    if ($("#inscription_prestataire_jour_3").is(":checked")) {
-        document.getElementById("add3").style.height = "auto";
-        document.getElementById("add3").append(document.getElementById("horraire-jeudi"));
-        document.getElementById("horraire-jeudi").style.visibility = "visible";
-    }
-    if (!$("#inscription_prestataire_jour_3").is(":checked")) {
-        document.getElementById("horraire-jeudi").style.visibility = "hidden";
-        document.getElementById("add3").style.height = "0";
+    if (!$("#profil_prestataire_jour_1").is(":checked")) {
+        document.getElementById("horaire-mardi").style.visibility = "hidden";
+        document.getElementById("horaire-mardi").style.display = "none";
+        /*document.getElementById("add1").style.height = "0";*/
     }
 
-    if ($("#inscription_prestataire_jour_4").is(":checked")) {
-        document.getElementById("add4").style.height = "auto";
-        document.getElementById("add4").append(document.getElementById("horraire-vendredi"));
-        document.getElementById("horraire-vendredi").style.visibility = "visible";
+    if ($("#profil_prestataire_jour_2").is(":checked")) {
+        /*document.getElementById("add2").style.height = "auto";
+        document.getElementById("add2").append(document.getElementById("horaire-mercredi"));*/
+        document.getElementById("horaire-mercredi").style.visibility = "visible";
+        document.getElementById("horaire-mercredi").style.display = "table-row"
     }
-    if (!$("#inscription_prestataire_jour_4").is(":checked")) {
-        document.getElementById("horraire-vendredi").style.visibility = "hidden";
-        document.getElementById("add4").style.height = "0";
-    }
-
-    if ($("#inscription_prestataire_jour_5").is(":checked")) {
-        document.getElementById("add5").style.height = "auto";
-        document.getElementById("add5").append(document.getElementById("horraire-samedi"));
-        document.getElementById("horraire-samedi").style.visibility = "visible";
-    }
-    if (!$("#inscription_prestataire_jour_5").is(":checked")) {
-        document.getElementById("horraire-samedi").style.visibility = "hidden";
-        document.getElementById("add5").style.height = "0";
+    if (!$("#profil_prestataire_jour_2").is(":checked")) {
+        document.getElementById("horaire-mercredi").style.visibility = "hidden";
+        document.getElementById("horaire-mercredi").style.display = "none";
+        /*document.getElementById("add2").style.height = "0";*/
     }
 
-    if ($("#inscription_prestataire_jour_6").is(":checked")) {
-        document.getElementById("add6").style.height = "auto";
-        document.getElementById("add6").append(document.getElementById("horraire-dimanche"));
-        document.getElementById("horraire-dimanche").style.visibility = "visible";
+    if ($("#profil_prestataire_jour_3").is(":checked")) {
+        /*document.getElementById("add2").style.height = "auto";
+        document.getElementById("add2").append(document.getElementById("horaire-mercredi"));*/
+        document.getElementById("horaire-jeudi").style.visibility = "visible";
+        document.getElementById("horaire-jeudi").style.display = "table-row"
     }
-    if (!$("#inscription_prestataire_jour_6").is(":checked")) {
-        document.getElementById("horraire-dimanche").style.visibility = "hidden";
-        document.getElementById("add6").style.height = "0";
+    if (!$("#profil_prestataire_jour_3").is(":checked")) {
+        document.getElementById("horaire-jeudi").style.visibility = "hidden";
+        document.getElementById("horaire-jeudi").style.display = "none";
+        /*document.getElementById("add2").style.height = "0";*/
+    }
+
+    if ($("#profil_prestataire_jour_4").is(":checked")) {
+        /*document.getElementById("add2").style.height = "auto";
+        document.getElementById("add2").append(document.getElementById("horaire-mercredi"));*/
+        document.getElementById("horaire-vendredi").style.visibility = "visible";
+        document.getElementById("horaire-vendredi").style.display = "table-row"
+    }
+    if (!$("#profil_prestataire_jour_4").is(":checked")) {
+        document.getElementById("horaire-vendredi").style.visibility = "hidden";
+        document.getElementById("horaire-vendredi").style.display = "none";
+        /*document.getElementById("add2").style.height = "0";*/
+    }
+
+    if ($("#profil_prestataire_jour_5").is(":checked")) {
+        /*document.getElementById("add2").style.height = "auto";
+        document.getElementById("add2").append(document.getElementById("horaire-mercredi"));*/
+        document.getElementById("horaire-samedi").style.visibility = "visible";
+        document.getElementById("horaire-samedi").style.display = "table-row"
+    }
+    if (!$("#profil_prestataire_jour_5").is(":checked")) {
+        document.getElementById("horaire-samedi").style.visibility = "hidden";
+        document.getElementById("horaire-samedi").style.display = "none";
+        /*document.getElementById("add2").style.height = "0";*/
+    }
+
+    if ($("#profil_prestataire_jour_6").is(":checked")) {
+        /*document.getElementById("add2").style.height = "auto";
+        document.getElementById("add2").append(document.getElementById("horaire-mercredi"));*/
+        document.getElementById("horaire-dimanche").style.visibility = "visible";
+        document.getElementById("horaire-dimanche").style.display = "table-row"
+    }
+    if (!$("#profil_prestataire_jour_6").is(":checked")) {
+        document.getElementById("horaire-dimanche").style.visibility = "hidden";
+        document.getElementById("horaire-dimanche").style.display = "none";
+        /*document.getElementById("add2").style.height = "0";*/
     }
 }
